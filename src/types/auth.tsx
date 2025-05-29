38c320db6e3e57d46435a633085b571d23350b7d
mothana <mothana@localhost.localdomain>
2025-05-29 19:46:37 +0300

    build registration functionality

---
+++ src/types/auth.tsx
@@ -0,0 +1,11 @@
+export interface registrationForm {
+    username: string,
+    email: string,
+    password: string,
+};
+export interface registrationFormErrors {
+    generic: string,
+    username: string,
+    email: string,
+    password: string,
+};
